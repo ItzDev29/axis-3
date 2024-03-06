c109441addb4bc6ce01fa91df3de3a98852f1aa1
ItzDev29 <92917373+ItzDev29@users.noreply.github.com>
2024-03-07 04:45:28 +0530

    Update page.jsx

--- src/app/Events/page.jsx
+++ src/app/Events/page.jsx
@@ -7,7 +7,7 @@ export default async function Page() {
 
    let Event;
     try {
-      const response = await fetch('https:/axis-3.vercel.app/api/getEvents', {
+      const response = await fetch('https://axis-3.vercel.app/api/getEvents', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
