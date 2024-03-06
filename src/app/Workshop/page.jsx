00d61bbd4dcaa1c50c1554162816f0575f0fb4f6
ItzDev29 <92917373+ItzDev29@users.noreply.github.com>
2024-03-07 04:08:55 +0530

    Update page.jsx

--- src/app/Workshop/page.jsx
+++ src/app/Workshop/page.jsx
@@ -9,7 +9,7 @@ export default async function Page() {
 
    let workshop={};
     try {
-      const response = await fetch('http://localhost:3000/api/getEvents', {
+      const response = await fetch('/api/getEvents', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
