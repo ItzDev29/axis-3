5cddbad58eb5ef4cf64ce46423fbe41a21d144ed
ItzDev29 <92917373+ItzDev29@users.noreply.github.com>
2024-03-07 04:48:07 +0530

    Update page.jsx

--- src/app/Sponsers/page.jsx
+++ src/app/Sponsers/page.jsx
@@ -3,7 +3,7 @@ export const dynamic='force-dynamic'
 export default async function Page() {
    let Event;
     try {
-      const response = await fetch('/api/getSponsers', {
+      const response = await fetch('https://axis-3.vercel.app/api/getSponsers', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
