3cfc34ed66785213e37c95598932c0197abc6f51
ItzDev29 <92917373+ItzDev29@users.noreply.github.com>
2024-03-07 04:32:33 +0530

    Update page.jsx

--- src/app/Events/page.jsx
+++ src/app/Events/page.jsx
@@ -7,7 +7,7 @@ export default async function Page() {
 
    let Event;
     try {
-      const response = await fetch(process.env.VERCEL_URL+'/api/getEvents', {
+      const response = await fetch(process.env.NEXT_PUBLIC_VERCEL_URL+'/api/getEvents', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
@@ -28,7 +28,7 @@ export default async function Page() {
     let User;
   if(userId){
    try {
-        const res = await fetch(process.env.VERCEL_URL+"/api/checkprofile", {
+        const res = await fetch(process.env.NEXT_PUBLIC_VERCEL_URL+"/api/checkprofile", {
           method: "GET",
           headers:{
                 "email":userId?.user.email
