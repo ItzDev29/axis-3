643e3b0745ed275a1b380b0e7bfad9d0954253da
ItzDev29 <92917373+ItzDev29@users.noreply.github.com>
2024-03-08 00:25:06 +0530

    Update page.jsx

--- src/app/sign-in/page.jsx
+++ src/app/sign-in/page.jsx
@@ -9,10 +9,7 @@ import Login from './SignIn';
 
 const StyledDiv = styled.div`
  z-index:1000;
- position: fixed;
- top: 50%;
- left: 50%;
- transform: translate(-50%, -50%);
+ position:sticky;
 `;
 export default  function Page() {
 
