af6da5f2c81e06093da992571086c96c91c172b8
ItzDev29 <92917373+ItzDev29@users.noreply.github.com>
2024-03-07 12:48:36 +0530

    Create [token].js

---
+++ src/app/verify/[token].js
@@ -0,0 +1,61 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
+export default function Page() {
+  const router = useRouter();
+  const { token } = router.query;
+  const [loading, setLoading] = useState(true);
+  const [verified, setVerified] = useState(false);
+
+  useEffect(() => {
+    const verifyToken = async () => {
+      try {
+        if (token) {
+          const response = await fetch('/api/verify-token', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              'token':token
+            },
+           
+          });
+          if (response.status === 200) {
+            setVerified(true);
+          }
+        }
+      } catch (error) {
+        console.error('Error verifying token:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    verifyToken();
+  }, [token]);
+
+  if(verified){
+    router.push("/")
+  }
+
+  return (
+    <>
+    
+    <div className="min-h-screen flex items-center justify-center">
+      {loading ? (
+        <div className="text-center">
+          <p className="text-lg font-semibold">Verifying...</p>
+
+        </div>
+      ) : (
+        <div className="text-center">
+          {verified ? (
+            <p className="text-lg font-semibold text-green-600">Account Verified.Redirecting</p>
+          ) : (
+            <p className="text-lg font-semibold text-red-600">Invalid token!</p>
+          )}
+        </div>
+      )}
+    </div>
+    </>
+  );
+}
