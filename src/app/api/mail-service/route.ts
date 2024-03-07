1f3f38fea1994493a713f86ff01e86fa9a7ab9ef
ItzDev29 <92917373+ItzDev29@users.noreply.github.com>
2024-03-07 12:23:17 +0530

    Update route.ts

--- src/app/api/mail-service/route.ts
+++ src/app/api/mail-service/route.ts
@@ -4,13 +4,15 @@ export async function POST(req:Request) {
   try {
     const {subject,email,text}=await req.json();
 
-       await sendMail(
+     const res=  await sendMail(
         subject,email,text
 
               );
+    console.log(res.message)
        return NextResponse.json( { message:"SUCCESS" },{status:200})
      
   } catch (error) {
+    console.log(error)
     return NextResponse.json(
       { message:error },
       { status: 500 }
