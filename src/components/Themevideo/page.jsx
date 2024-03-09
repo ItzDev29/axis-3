6c94c861bbd3d4d29d196340a8d2135cfef91652
ItzDev29 <92917373+ItzDev29@users.noreply.github.com>
2024-03-09 19:12:08 +0530

    Update page.jsx

--- src/components/Themevideo/page.jsx
+++ src/components/Themevideo/page.jsx
@@ -2,13 +2,13 @@ export default function Theme() {
   return (
      <>
        <div className="flex justify-center items-center">
-       <h1 className="text-3xl sm:text-4xl md:text-6xl mt-3 mb-3 font-bold">AXIS-24 Theme Video</h1>
+         <h1 className="text-3xl sm:text-4xl md:text-6xl mt-3 mb-3 font-bold">AXIS-24 Theme Video</h1>
        </div>
        <div className="flex justify-center items-center mt-10">
          <div className="w-full aspect-[16/9]">
            <iframe
              className="w-full h-full"
-             src={`https://www.youtube.com/embed/IXnmWNu6_4o?si=FRCwziHsZwHTDgfv`}
+             src="https://www.youtube.com/embed/IXnmWNu6_4o?autoplay=1&mute=1"
              title="YouTube video player"
              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
              allowFullScreen
