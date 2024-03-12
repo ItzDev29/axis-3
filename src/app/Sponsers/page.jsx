c4a3d6b61efb1a5bacbc4e5ffb50cbe5c7719988
ItzDev29 <92917373+ItzDev29@users.noreply.github.com>
2024-03-13 03:13:41 +0530

    Update page.jsx

--- src/app/Sponsers/page.jsx
+++ src/app/Sponsers/page.jsx
@@ -9,28 +9,32 @@ export default function Sponsers() {
   
   const [loading, setLoading] = useState(true);
  const[data,setData]=useState({});
-  useEffect(async ()=>{
-    try{
+ useEffect(() => {
+    const fetchData = async () => {
+      try {
         const response = await fetch('/api/getSponsers', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-       
-        },
-     
-      });
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch sponsors');
+        }
+
+        const sponsors = await response.json();
+        setData(sponsors.events[0]);
+        setLoading(false);
+      } catch (error) {
+        console.log('Error fetching sponsors:', error);
+        setLoading(false);
       
-      if (!response.ok) {
-        throw new Error('Failed to fetch events');
       }
-      
-      const events = await response.json();
-       Event= events && events.events;
-       setData(Event[0]);
-    } catch (error) {
-      console.log('Error fetching events:', error);
-    }
-  });
+    };
+
+    fetchData();
+  }, []);
     useEffect(() => {
     if (data) {
       setLoading(false);
